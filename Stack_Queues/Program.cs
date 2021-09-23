7bf6847637debff25a15ef809047978477341bbc
samiksha-chaudhari <spchaudhari80@gmail.com>
2021-09-23 22:35:38 +0530

    create queue

--- Stack_Queues/Program.cs
+++ Stack_Queues/Program.cs
@@ -8,13 +8,19 @@ namespace Stack_Queues
         {
             Console.WriteLine("Welcome to stack and Queues");
             Stack<int> Stack = new Stack<int>();
-            Stack.Push(70);
-            Stack.Push(30);
-            Stack.Push(56);
-            Stack.Display();
-            Stack.Peek();
-            Stack.Pop();
-            Stack.Empty();
+            /* Stack.Push(70);
+             Stack.Push(30);
+             Stack.Push(56);
+             Stack.Display();
+             Stack.Peek();
+             Stack.Pop();
+             Stack.Empty();*/
+
+            Queue<int> Queue = new Queue<int>();
+             Queue.Enqueue(70);
+             Queue.Enqueue(30);
+             Queue.Enqueue(56);
+             Queue.Display();
         }
     }
 }
---
+++ Stack_Queues/Queue.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Stack_Queues
+{
+    class Queue<Gtype>
+    {
+        Node<Gtype> head = null;
+        internal void Enqueue(Gtype data)
+        {
+            Node<Gtype> node = new Node<Gtype>(data);
+            if (head == null)
+                head = node;
+            else
+            {
+                Node<Gtype> temp = head;
+                while (temp.next != null)
+                {
+                    temp = temp.next;
+                }
+                temp.next = node;
+            }
+            Console.WriteLine("{0} added", node.data);
+        }
+
+        internal void Display()
+        {
+            Node<Gtype> temp = this.head;
+            if (temp == null)
+            {
+                Console.WriteLine("Queue is empty");
+                return;
+            }
+            while (temp != null)
+            {
+                Console.Write(temp.data + " ");
+                temp = temp.next;
+            }
+        }
+    }
+}
