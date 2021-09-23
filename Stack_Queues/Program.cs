80d6d55d9afb16f29426eda26ecea5aa2cd5d093
samiksha-chaudhari <spchaudhari80@gmail.com>
2021-09-23 18:00:59 +0530

    welcome

---
+++ Stack_Queues/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Stack_Queues
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to stack and Queues");
+        }
+    }
+}
