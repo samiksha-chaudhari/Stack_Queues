cd0d4a7eec96192f4de0792bebb2863663195143
samiksha-chaudhari <spchaudhari80@gmail.com>
2021-09-23 21:45:41 +0530

    adding number to stack

--- Stack_Queues/Program.cs
+++ Stack_Queues/Program.cs
@@ -7,6 +7,11 @@ namespace Stack_Queues
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to stack and Queues");
+            Stack Stack = new Stack();
+            Stack.Push(70);
+            Stack.Push(30);
+            Stack.Push(56);
+            Stack.Display();
         }
     }
 }
---
+++ Stack_Queues/Stack.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Stack_Queues
+{
+    class Stack
+    {
+        private Node top;
+        public Stack()
+        {
+            this.top = null;
+        }
+        public void Push(int no)
+        {
+            Node node = new Node(no);
+            if (this.top == null)
+                node.next = null;
+            else
+                node.next = top;
+            this.top = node;
+            Console.WriteLine("{0} pushed", no);
+        }
+        public void Display()
+        {
+            Node temp = this.top;
+            while (temp != null)
+            {
+                Console.Write(temp.data + " ");
+                temp = temp.next;
+            }
+        }
+    }
+}
