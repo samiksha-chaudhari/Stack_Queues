8965f0e5b339ea936ff4f95f83d9b45803fec914
samiksha-chaudhari <spchaudhari80@gmail.com>
2021-09-24 16:06:55 +0530

    adding switch case

--- Stack_Queues/Program.cs
+++ Stack_Queues/Program.cs
@@ -2,29 +2,46 @@
 
 namespace Stack_Queues
 {
-    class Program
+  class Program
     {
-        static void Main(string[] args)
+     static void Main(string[] args)
+     {
+        Console.WriteLine("Welcome to stack and Queues");        
+        int count = 0;//counter to break the while loop
+        while (count == 0)
         {
-            Console.WriteLine("Welcome to stack and Queues");
-            Stack<int> Stack = new Stack<int>();
-            /* Stack.Push(70);
-             Stack.Push(30);
-             Stack.Push(56);
-             Stack.Display();
-             Stack.Peek();
-             Stack.Pop();
-             Stack.Empty();*/
-
-            Queue<int> Queue = new Queue<int>();
-            Queue.Enqueue(70);
-            Queue.Enqueue(30);
-            Queue.Enqueue(56);
-            Console.WriteLine("before dequeue : ");
-            Queue.Display();
-            Queue.Dequeue();
-            Console.WriteLine("\nafter dequeue : ");
-            Queue.Display();
+           Console.WriteLine("1:Stack  2:Queue  3:Exit");
+           int choice = Convert.ToInt32(Console.ReadLine());//variable for taking choice from the user
+           Console.WriteLine("\n");
+           switch (choice)  //switch case
+           {
+               case 1:
+                   Stack<int> Stack = new Stack<int>();
+                   Stack.Push(70);
+                   Stack.Push(30);
+                   Stack.Push(56);
+                   Stack.Display();
+                   Stack.Peek();
+                   Stack.Pop();
+                   Stack.Empty();
+                   break;
+                case 2:
+                   Queue<int> Queue = new Queue<int>();
+                   Queue.Enqueue(70);
+                   Queue.Enqueue(30);
+                   Queue.Enqueue(56);
+                   Console.WriteLine("before dequeue : ");
+                   Queue.Display();
+                   Queue.Dequeue();
+                   Console.WriteLine("\nafter dequeue : ");
+                   Queue.Display();
+                   break;
+                 default:
+                   Console.WriteLine("Exit");//default condition
+                   count++;
+                   break;
+           }
         }
-    }
+     }
+  }
 }
